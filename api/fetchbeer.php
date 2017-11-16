2224cc305c239f3a676f501ebebd84120d8efdf7
Savpek <savolainen.pekka@gmail.com>
2017-11-17 01:19:19 +0200

    Fubar

--- api/fetchbeer.php
+++ api/fetchbeer.php
@@ -8,7 +8,7 @@ if (!$ssh->login($username, $password)) {
 }
 
 $ssh->exec('cd /var/temp/torch-rnn');
-$beerNames = $ssh->exec('th sample.lua -checkpoint cv/checkpoint_9000.t7 -length 2000 -gpu -1');
+$beerNames = $ssh->exec('/root/torch/install/bin/th sample.lua -checkpoint cv/checkpoint_9000.t7 -length 2000 -gpu -1');
 $beerNamesArray = explode("\n", $beerNames);
 
 echo json_encode($beerNamesArray);
